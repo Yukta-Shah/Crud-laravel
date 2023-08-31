5adc0f69c9f61cc2502ec3d4eb651138cd7f2880
Yukta Shah <140506091+Yukta-Shah@users.noreply.github.com>
2023-08-31 13:40:17 +0530

    Create create.blade.php

---
+++ resource/views/users/create.blade.php
@@ -0,0 +1,36 @@
+@extends('users.layout')
+
+@section('content')
+    <div class="container">
+        <h2>Users</h2>
+        <a href="{{ route('users.create') }}" class="btn btn-primary">Add User</a>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->mobile }}</td>
+                        <td>
+                            <a href="{{ route('users.show', $user->id) }}" class="btn btn-info">View</a>
+                            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary">Edit</a>
+                            <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
